760f5b03bf0cb743f438e3319a1030cf821acd77
gersonup <gersonup@gmail.com>
2022-05-20 11:54:48 -0500

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Felipe Rolvar on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
